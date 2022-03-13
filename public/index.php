4296519e4bf73244f76ebb07f2c8fc05aab51c64
James Holloway <jholl810@mtroyal.ca>
2022-03-13 11:08:04 -0600

    please

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+//this is a comment
+function makeh1()
+{
+  return "<h1>It's alive</h1>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?= makeh1(); ?>
+</body>
+
+</html>
