7a4dd416b43b11031f4c9d203828b284c6df4ada
tmtkachyk <tmtkachyk@gmail.com>
2022-03-13 18:38:59 -0600

    [ABOUT.2]&[ABOUT.3] display time & time remaining

--- public/about.php
+++ public/about.php
@@ -10,17 +10,22 @@
 
 <body>
   <?php
-  $dueDate = mktime(21, 0, 0, 4, 6, 2022); //April 6, 2014 21:00:00
-  $today = time(); // current time in seconds since 1970.
-  $timeRemaining = $dueDate - $today;
+  date_default_timezone_set("America/Edmonton");
+  $dueDate = mktime(21, 0, 0, 4, 6, 2022); //April 6, 2022 21:00:00
+  $dueDateRegular = date("h:i a D, M d, Y", $dueDate);
+  $todayTime = time(); // current time in seconds since 1970.
+  $todayDate = date("D, M d, Y");
+  $currentTime = date("h:i a");
+  $timeRemaining = $dueDate - $todayTime;
 
-  echo "<p>Todays date is" . date("M d, Y", $today) .
-    ":</p>";
-  echo "<p>Milestone 5 is due on" . date("M d, Y", $dueDate) .
-    ":</p>";
-  echo "<p>Time remaining" . date("M d, Y", $timeRemaining) .
-    ":</p>";
+  $daysRemaining = round($timeRemaining / (60 * 60 * 24));
+  $hoursRemaining = round(($timeRemaining - ($daysRemaining * 60 * 60 * 24)) / (60 * 60));
+  $minutesRemaining = round(($timeRemaining - ($hoursRemaining * 60 * 60) - ($daysRemaining * 60 * 60 * 24)) / (60));
 
+
+  echo "<p>It is currently $currentTime on $todayDate in Alberta</p>";
+  echo "<p>The due date for Milestone #5 is: $dueDateRegular</p>";
+  echo "<p>There are $daysRemaining days, $hoursRemaining hours and $minutesRemaining minutes remaining until Milestone #5 is due </p>";
   ?>
 </body>
 
