533367c07e1e05007848c0dad48547456f06e272
James Holloway <jholl810@mtroyal.ca>
2022-03-23 16:10:19 -0600

    adding config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'database' => [
+    'name' => $_ENV['DATABASE_NAME'] ?? 'movies',
+    'username' => $_ENV['DATABASE_USER'] ?? 'root',
+    'password' => $_ENV['DATABASE_PW'] ?? '',
+    'connection' => $_ENV['DATABASE_CONNECTION'] ?? 'mysql:host=34.130.236.198'
+  ]
+];
