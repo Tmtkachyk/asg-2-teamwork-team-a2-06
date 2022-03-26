a752fea50a7ea36d8fc97c669e746524dd440aa9
mhach145 <46723180+mhach145@users.noreply.github.com>
2022-03-26 13:26:07 -0600

    testing ID as string query

--- public/single-movie.php
+++ public/single-movie.php
@@ -15,6 +15,9 @@ if (isset($_GET["id"])) {
   $resultingIDs = $idStatement->fetchAll(PDO::FETCH_ASSOC);
 
 
+   if (is_string($id)){
+      header("Location: error.php");
+   }
 
   if (count($_GET) > 1) {
     header("Location: error.php");
@@ -25,6 +28,7 @@ if (isset($_GET["id"])) {
   if (ctype_upper($id)) {
     header("Location: error.php");
   }
+
 } else {
   header("Location: error.php");
 }
