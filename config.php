71d5c2cd28e9303d702f8b5e72b76259564c0380
tmtkachyk <tmtkachyk@gmail.com>
2022-03-30 01:10:19 -0600

    config merge adjustment

--- config.php
+++ config.php
@@ -5,17 +5,5 @@ return [
     'username' => $_ENV['DATABASE_USER'] ?? 'root',
     'password' => $_ENV['DATABASE_PW'] ?? '',
     'connection' => $_ENV['DATABASE_CONNECTION'] ?? 'mysql:host=localhost'
-<<<<<<< HEAD
   ]
-=======
-  ],
-
-  'users' => [
-    'name' => $_ENV['DATABASE_NAME'] ?? 'users',
-    'username' => $_ENV['DATABASE_USER'] ?? 'root',
-    'password' => $_ENV['DATABASE_PW'] ?? '',
-    'connection' => $_ENV['DATABASE_CONNECTION'] ?? 'mysql:host=localhost'
-  ]
-
->>>>>>> 7908d819d2ebe6a526921ee1c4f5eeeed879613f
 ];
