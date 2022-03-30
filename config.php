65b50ee38f6be7f250cf5c87ffcedb2bd2a9c918
mhach145 <mhach145@mtroyal.ca>
2022-03-29 23:12:14 -0600

    login is now working - input needs sanitation

--- config.php
+++ config.php
@@ -5,10 +5,7 @@ return [
     'username' => $_ENV['DATABASE_USER'] ?? 'root',
     'password' => $_ENV['DATABASE_PW'] ?? '',
     'connection' => $_ENV['DATABASE_CONNECTION'] ?? 'mysql:host=localhost'
-<<<<<<< HEAD
-  ]
-=======
-  ], //host=34.130.236.198
+  ],
 
   'users' => [
     'name' => $_ENV['DATABASE_NAME'] ?? 'users',
@@ -17,5 +14,4 @@ return [
     'connection' => $_ENV['DATABASE_CONNECTION'] ?? 'mysql:host=localhost'
   ]
 
->>>>>>> 7dab040b3016f083fd78d76ae4e178292179abaf
 ];
