35ba4c552f8afdd7b8d23e5517b3ca9d27f045e4
mhach145 <mhach145@mtroyal.ca>
2022-03-31 21:49:00 -0600

    added header and style to Fav page

--- public/favourites.php
+++ public/favourites.php
@@ -1,12 +1,85 @@
+<?php
+    
+      
+  ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Favourites</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="loginStyle.css">
+
+  <script src="header.js"></script>
+  <link rel="stylesheet" href="headerStyle.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+
+  <script>
+    tailwind.config = {
+
+        fontFamily: {
+          Times: ["Times New Roman", "sans-serif"],
+          Arial: ["Arial", "sans-serif"],
+          open: ["Open Sans", "serif"],
+          source: ["Source Sans Pro", "sans-serif"],
+          montser: ["Montserrat", "serif"],
+        },
+        screens: {
+          'sm': {
+            'max': '650px'
+          },
+          md: "768px",
+          lg: "1440px",
+          // maxSC: {
+          //   max: "426px"
+          // },
+        },
+
+        extend: {},
+      height: {
+        128: "32rem",
+      },
+      plugins: [],
+    };
+  </script>
+
 </head>
-<body>
-    <h1>Favourites will go here</h1>
+
+
+
+<body class="bg-image-1 transition-all ease-linear duration-[3000ms] bg-cover bg-center bg-fixed">
+
+  <!-- <div class="headerDiv">
+  </div>
+  <div class="smallMenu">
+  </div> -->
+
+  <div class="flex flex-col justify-center m-0 h-[100vh] items-center font-open" id="homePage">
+
+
+    <?php include 'header.php';
+    ?>
+    <main class="container m-auto h-[75vh]">
+
+
+        <div class="imgcontainer">
+          <img src="../images/favIcon.png" alt="Login Icon" class="loginIcon" style="height: 200px">
+        </div>
+
+        <h1 style="text-align:center; color:red"> Favourites will go here </h1>
+
+       
+    </main>
+
+  </div>
 </body>
+
 </html>
