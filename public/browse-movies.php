14f2169d4c200e3cf3e84636c5ec87258093b6ff
mhach145 <mhach145@mtroyal.ca>
2022-04-02 13:32:04 -0600

    another eroku test

--- public/browse-movies.php
+++ public/browse-movies.php
@@ -149,19 +149,17 @@
                 </div>
 
                 <div class="col-span-5">
-                  <div class="grid grid-cols-5 gap-2" id="defaultInfo">
-
-                  <?php
-
-                  if(isset($_SESSION['log']) && $_SESSION['log'] == 'in')
-                  {
-                    echo '<div class="ghettoFix"> </div>';
-                  }
-                  
-                  ?>
-
-                  </div>
+                  <div class="grid grid-cols-5 gap-2" id="defaultInfo"></div>
                 </div>
+
+                                                <?php
+
+                                if(isset($_SESSION['log']) && $_SESSION['log'] == 'in')
+                                {
+                                  echo '<div class="ghettoFix"> </div>'
+                                }
+
+                                ?>
 
                 <div class="col-span-5 flex justify-center font-montser text-5xl hidden" id="noResultsFilter">
                   No Filtered Movies
