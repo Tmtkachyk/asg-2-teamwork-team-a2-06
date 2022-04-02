92814aeb890589c7e358b859a186bf905f585d35
tmtkachyk <tmtkachyk@gmail.com>
2022-04-02 17:10:50 -0600

    testing search

--- public/search.js
+++ public/search.js
@@ -3,53 +3,53 @@ let searchButton = document.querySelector("#homeSearchButton")
 let homeSearchBox = document.querySelector("#homeSearchBox");
 
 window.addEventListener("load", () => {
-  searchButton.value = "";
-  searchButton.disabled = true;
+  // searchButton.value = "";
+  // searchButton.disabled = true;
   clearEverything();
 });
 
 
 //Enter key to search
-homeSearchBox.addEventListener("keyup", function(event) {
-  if (event.keyCode == 13) {
-    if(homeSearchBox != ""){
-    let userSearch = homeSearchBox.value;
-    fetchingMatchingMovies(userSearch);
-    }
-  }
-});
+// homeSearchBox.addEventListener("keyup", function(event) {
+//   if (event.keyCode == 13) {
+//     if(homeSearchBox != ""){
+//     let userSearch = homeSearchBox.value;
+//     fetchingMatchingMovies(userSearch);
+//     }
+//   }
+// });
 
 //[HOME.4] A user should not be allowed to search for a title when the title search box is empty.
-homeSearchBox.addEventListener("input", () => {
-  if (homeSearchBox.value == "") {
-    searchButton.disabled = true;
-  } else {
-    searchButton.disabled = false;
-}
-});
-
-searchButton.addEventListener("click", () => {
-let userSearch = homeSearchBox.value;
-fetchingMatchingMovies(userSearch);
-showLoadingAnimation();
-})
-
-
-//A user should not be allowed to search for a title when the title search box is empty.
 // homeSearchBox.addEventListener("input", () => {
 //   if (homeSearchBox.value == "") {
 //     searchButton.disabled = true;
-//     console.log("empty");
 //   } else {
-  //Search movie through click
-// searchButton.addEventListener("click", () => {
-//   let userSearch = homeSearchBox.value;
-//   fetchingMatchingMovies(userSearch);
-//   })
-
-
+//     searchButton.disabled = false;
 // }
 // });
+
+// searchButton.addEventListener("click", () => {
+// let userSearch = homeSearchBox.value;
+// fetchingMatchingMovies(userSearch);
+
+// });
+
+
+//A user should not be allowed to search for a title when the title search box is empty.
+homeSearchBox.addEventListener("input", () => {
+  if (homeSearchBox.value == "") {
+    searchButton.disabled = true;
+    console.log("empty");
+  } else {
+  //Search movie through click
+searchButton.addEventListener("click", () => {
+  let userSearch = homeSearchBox.value;
+  fetchingMatchingMovies(userSearch);
+  })
+
+
+}
+});
 
 
  
