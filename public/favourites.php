90e85e453f6154883e6d2e65cef6264666c27fe9
tmtkachyk <tmtkachyk@gmail.com>
2022-04-04 11:28:58 -0600

    favourites start

--- public/favourites.php
+++ public/favourites.php
@@ -1,29 +1,31 @@
-<?php
-    
-      
-  ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-
-  <link rel="stylesheet" href="loginStyle.css">
-
-  <script src="header.js"></script>
-  <link rel="stylesheet" href="headerStyle.css">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- <meta name="Author" content="" /> -->
+  <title>HomePage</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="headerStyle.css">
+  <script src="header.js"></script>
+
+
 
   <script>
     tailwind.config = {
+      theme: {
+        backgroundImage: {
+          "image-1": "url('../images/2049SS.jpg')",
+          "image-2": "url('../images/titanicSS.jpg')",
+          "image-3": "url('../images/interstellarSS.jpg')",
+          "image-4": "url('../images/jokerSS.jpg')",
+          "image-5": "url('../images/alienSS.png')",
+        },
 
         fontFamily: {
           Times: ["Times New Roman", "sans-serif"],
@@ -44,6 +46,7 @@
         },
 
         extend: {},
+      },
       height: {
         128: "32rem",
       },
@@ -57,29 +60,25 @@
 
 <body class="bg-image-1 transition-all ease-linear duration-[3000ms] bg-cover bg-center bg-fixed">
 
-  <!-- <div class="headerDiv">
-  </div>
-  <div class="smallMenu">
-  </div> -->
 
   <div class="flex flex-col justify-center m-0 h-[100vh] items-center font-open" id="homePage">
-
-
     <?php include 'header.php';
     ?>
     <main class="container m-auto h-[75vh]">
 
+      <?php
+      if (isset($_SESSION['log']) && $_SESSION['log'] == 'in') {
+        //echo '<li class="bg-black/80 text-white"><a href="logout.php">Log out</a></li>';
+        echo "you belong here";
+      } else {
+        echo "you don't belong here";
+      }
+      ?>
 
-        <div class="imgcontainer">
-          <img src="../images/favIcon.png" alt="Login Icon" class="loginIcon" style="height: 200px">
-        </div>
-
-        <h1 style="text-align:center; color:red"> Favourites will go here </h1>
-
-       
     </main>
-
   </div>
 </body>
+<script src="search.js"></script>
+<script src="movieHelpers.js"></script>
 
 </html>
