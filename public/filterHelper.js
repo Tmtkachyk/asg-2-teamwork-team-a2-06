5ab4c309f3436ffdc874fe50a81bc2134d2cd262
tmtkachyk <tmtkachyk@gmail.com>
2022-04-06 23:53:02 -0600

    clean up on aisle filterHelper.js

--- public/filterHelper.js
+++ public/filterHelper.js
@@ -1,95 +1,89 @@
 let filterButton = document.querySelector("#filterButton");
 let resetButton = document.querySelector("#resetButton");
    
+//click event listener on filter button
    filterButton.addEventListener("click", () => {
-
-
     filterMovies();
     displayFilteredMovies();
-
-
   })
 
+//click event listener on reset button
   resetButton.addEventListener("click", () => {
-    
-   resetFilters();
-
-
+   resetFilter();
   })
 
-// function titleFilter(listOfMovies){
-//   let sortedMovies = sortAlpha(listOfMovies);
-//    return sortedMovies;
-//    }
+
 
 
 function filterMovies(){
-  //gets it as a normal array
-  //console.log(JSON.parse(sessionStorage.getItem("filteredMovies"))[0].title);
-  //working
-  //sessionStorage.setItem("filteredMovies", sessionStorage.getItem("matchingMovies"));
 
   //pulls out matchingMovies session storage into an array
   let preFilterArray = JSON.parse(sessionStorage.getItem("matchingMovies"));
 
-  //filter the array
-  //working vote_average array
-  //let postFilterMovieArray = preFilterArray.filter(movie => movie.vote_average > 5 );
+  //Title Filter
+  titleFilter(preFilterArray);
+  //Below rating filter
+  belowRatingfilter(preFilterArray);  
+  //Above rating filter
+  aboveRatingFilter(preFilterArray);
 
+}
 
-//Title Filter
-
-  //Before Year Filter
+//TITLE filter
+function titleFilter(preFilterArray){
   if(document.getElementById('radioTitle').checked) {
-
     let titleFilterValue = document.getElementById('titleFilter').value.toLowerCase();
     let postFilterMovieArray = preFilterArray.filter(movie => movie.title.toLowerCase().includes(titleFilterValue));
     //put filtered movies into session storage filteredMovies
   sessionStorage.setItem("filteredMovies", JSON.stringify(postFilterMovieArray));
-
-
-  // filtered = myArray.filter(function (str) { return str.includes(PATTERN); });
   }
+}  
 
-  //Below rating filter
+//YEAR FILTERS BELOW
+//before year filter
+function beforeYearFilter() {
+  return;
+}
+
+//after year filter
+function afterYearFilter() {
+  return;
+}
+
+//between year filter
+function betweenYearFilter() {
+  return;
+}
+
+
+//RATING FILTERS BELOW
+//below rating filter
+function belowRatingfilter(preFilterArray) {
   if(document.getElementById('radioBelow').checked) {
- 
     let belowFilterValue = document.getElementById('belowRating').value;
     let postFilterMovieArray = preFilterArray.filter(movie => movie.vote_average < belowFilterValue );
       //put filtered movies into session storage filteredMovies
     sessionStorage.setItem("filteredMovies", JSON.stringify(postFilterMovieArray));
   }
+}
 
-  //Above rating filter
+//above rating filter
+function aboveRatingFilter(preFilterArray) {
   if(document.getElementById('radioAbove').checked) {
- 
     let aboveFilterValue = document.getElementById('aboveRating').value;
     let postFilterMovieArray = preFilterArray.filter(movie => movie.vote_average > aboveFilterValue );
       //put filtered movies into session storage filteredMovies
     sessionStorage.setItem("filteredMovies", JSON.stringify(postFilterMovieArray));
-    console.log(aboveFilterValue);
   }
-  
-
-  
-  
 }
 
-//reset filters
-function resetFilters(){
-  clearDefault();
-  sessionStorage.removeItem("filteredMovies");
-
- if (JSON.parse(sessionStorage.getItem("matchingMovies")).length > 0) {
-  document.querySelector("#noMovies").classList.add("hidden");
- } ;
-
-  displayMatchingMovies();
+//Inbetween rating filter
+function inbetweenRatingFilter() {
+  return;
 }
 
-
-function displayFilteredMovies(){
-      
+//Displays the filtered movies from session storage onto the page
+function displayFilteredMovies(){  
   let listOfMovies = JSON.parse(sessionStorage.getItem("filteredMovies"));
 
   if (listOfMovies.length == 0 || listOfMovies == null ) {
@@ -99,18 +93,49 @@ function displayFilteredMovies(){
     clearDefault();
     listOfMovies.forEach(movie =>{
       populateDefault(movie);
-      });
-    
-    //populateDefault(listOfMovies[0]);
-    
+      }); 
   }};
 
 
+//After reset button is clicked sets everything back to starting
+function resetFilter(){
+  clearDefault();
+  clearFilters();
+  sessionStorage.removeItem("filteredMovies");
+
+ if (JSON.parse(sessionStorage.getItem("matchingMovies")).length > 0) {
+  document.querySelector("#noMovies").classList.add("hidden");
+ } ;
+
+  displayMatchingMovies();
+}
+
+//resets the input filter values
+function clearFilters() {
+  document.getElementById('filterForm').reset();
+}
+
+//clears all the movie details i.e. poster, title, date and rating
   function clearDefault() {
-
-
    while (document.querySelector("#defaultInfo").firstChild) {
     document.querySelector("#defaultInfo").removeChild(document.querySelector("#defaultInfo").lastChild);
   }
-  
   };
+
+
+
+
+  //gets it as a normal array
+  //console.log(JSON.parse(sessionStorage.getItem("filteredMovies"))[0].title);
+  //working
+  //sessionStorage.setItem("filteredMovies", sessionStorage.getItem("matchingMovies"));
+  //filter the array
+  //working vote_average array
+  //let postFilterMovieArray = preFilterArray.filter(movie => movie.vote_average > 5 );
+
+
+
+  // function titleFilter(listOfMovies){
+  //   let sortedMovies = sortAlpha(listOfMovies);
+  //    return sortedMovies;
+  //    }
