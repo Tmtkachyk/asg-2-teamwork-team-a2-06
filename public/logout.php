fd6c9ab485770356e96a141f6b8a88843a37aa52
James Holloway <jholl810@mtroyal.ca>
2022-04-08 17:54:05 -0600

    broken

--- public/logout.php
+++ public/logout.php
@@ -1,7 +1,22 @@
 <?php
-    session_start();
-    $_SESSION["log"] = 'out';
-    $_POST["email"] = "";
-    $_POST["password"] = "";
+session_start();
+$config = include_once "../config.php";
+include "../database/Connection.php";
+$user_email = $_SESSION["email"];
+$favList = $_SESSION["favs"];
+var_dump($favList);
+$favList = json_encode($favList);
+var_dump($favList);
+$pdo2 = Connection::connect($config['database']);
+$favListUpdateQuery = "REPLACE INTO favourites_list (user_email, favourites_list) VALUES ($user_email, "{"1789":{"title":"Alien","posterPath":"\/bk9GVjN4kxmGekswNigaa5YIdr5.jpg"}}")";
+$favStatment = $pdo2->prepare($favListUpdateQuery);
+$favStatment->execute();
 
-    header("location:index.php");
+$_SESSION['favs'] = [];
+
+
+$_SESSION["log"] = 'out';
+$_POST["email"] = "";
+$_POST["password"] = "";
+
+header("location:index.php");
