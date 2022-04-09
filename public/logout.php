ee0d3fc70d8c294d1612ba89ff47473f861e661f
James Holloway <jholl810@mtroyal.ca>
2022-04-08 18:18:59 -0600

    test123412333

--- public/logout.php
+++ public/logout.php
@@ -3,14 +3,15 @@ session_start();
 $config = include_once "../config.php";
 include "../database/Connection.php";
 $user_email = $_SESSION["email"];
+var_dump($user_email);
 $favList = $_SESSION["favs"];
 var_dump($favList);
 $favList = json_encode($favList);
 var_dump($favList);
 $pdo2 = Connection::connect($config['database']);
-$favListUpdateQuery = "REPLACE INTO favourites_list (user_email, favourites_list) VALUES ($user_email, $favList)";
+$favListUpdateQuery = "REPLACE INTO favourites (user_email, favourites_list) VALUES ('$user_email', :favList)";
 $favStatment = $pdo2->prepare($favListUpdateQuery);
-$favStatment->execute();
+$favStatment->execute(["favList" => $favList]);
 
 $_SESSION['favs'] = [];
 
@@ -19,4 +20,4 @@ $_SESSION["log"] = 'out';
 $_POST["email"] = "";
 $_POST["password"] = "";
 
-//header("location:index.php");
+header("location:index.php");
