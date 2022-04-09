0b69dda4a1f24eb1fd51ecf1c170887c6f477dcd
James Holloway <jholl810@mtroyal.ca>
2022-04-08 18:32:28 -0600

    log out test

--- public/logout.php
+++ public/logout.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-$config = include_once "../config.php";
-include "../database/Connection.php";
-$user_email = $_SESSION["email"];
-$favList = $_SESSION["favs"];
-$favList = json_encode($favList);
-$pdo2 = Connection::connect($config['database']);
-$favListUpdateQuery = "REPLACE INTO favourites (user_email, favourites_list) VALUES ('$user_email', :favList)";
-$favStatment = $pdo2->prepare($favListUpdateQuery);
-$favStatment->execute(["favList" => $favList]);
+// $config = include_once "../config.php";
+// include "../database/Connection.php";
+// $user_email = $_SESSION["email"];
+// $favList = $_SESSION["favs"];
+// $favList = json_encode($favList);
+// $pdo2 = Connection::connect($config['database']);
+// $favListUpdateQuery = "REPLACE INTO favourites (user_email, favourites_list) VALUES ('$user_email', :favList)";
+// $favStatment = $pdo2->prepare($favListUpdateQuery);
+// $favStatment->execute(["favList" => $favList]);
 
-$_SESSION['favs'] = [];
+// $_SESSION['favs'] = [];
 
 
 $_SESSION["log"] = 'out';
